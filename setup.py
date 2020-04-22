26d2d82add418ec163aea604832644893a9ae690
hw <hussainweb@gmail.com>
2020-04-21 20:12:28 -0400

    Release v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="axl-template",  # Replace with your own username
-    version="0.1.4",
+    version="0.1.5",
     author="hussainweb",
     author_email="hussainweb@gmail.com",
     description="Scaffold a Drupal site template",
