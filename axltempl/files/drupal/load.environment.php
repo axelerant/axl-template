01981edad1119842dc215eed5fa61007a900b626
hw <hussainweb@gmail.com>
2020-07-08 15:18:36 -0400

    Update to phpdotenv 5.0

--- axltempl/files/drupal/load.environment.php
+++ axltempl/files/drupal/load.environment.php
@@ -6,10 +6,9 @@
  */
 
 use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
 
 /**
  * Load any .env file. See /.env.example.
  */
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->safeLoad();
