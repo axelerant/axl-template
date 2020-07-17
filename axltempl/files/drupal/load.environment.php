8db763832225ceb9bf0e11f7e5d73febd2ae8379
Taher <taher@axelerant.com>
2020-07-17 12:01:12 +0530

    fix: phpcs errors on initial commits

--- axltempl/files/drupal/load.environment.php
+++ axltempl/files/drupal/load.environment.php
@@ -1,5 +1,6 @@
 <?php
 
+// @codingStandardsIgnoreFile
 /**
  * This file is included very early. See autoload.files in composer.json and
  * https://getcomposer.org/doc/04-schema.md#files
--- axltempl/files/drupal/settings.env.php
+++ axltempl/files/drupal/settings.env.php
@@ -1,5 +1,6 @@
 <?php
 
+// @codingStandardsIgnoreFile
 if (!getenv('MYSQL_DATABASE')) {
   return;
 }
