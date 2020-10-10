111ff59e58d73d976631d53d147ffe105dcba649
hw <hussainweb@gmail.com>
2020-10-10 19:07:18 -0400

    chore: move to declarative config for setuptools

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="axl-template",  # Replace with your own username
-    version="0.4.5",
-    author="hussainweb",
-    author_email="hussainweb@gmail.com",
-    description="Scaffold a Drupal site template",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/axelerant/axl-template",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords="drupal",
-    include_package_data=True,
-    install_requires=["Click",],
-    entry_points={
-        "console_scripts": [
-            "init-drupal = axltempl.drupal:main",
-            "init-lando = axltempl.lando:main",
-            "init-gitlab = axltempl.gitlab:main",
-        ]
-    },
-    python_requires=">=3.6",
-)
+setuptools.setup()
