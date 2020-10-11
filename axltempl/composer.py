2fdab5fec0b5b8fcd1b660f231f71e0332311aec
hw <hussainweb@gmail.com>
2020-10-10 22:12:48 -0400

    refactor: move all composer related code into a utility class

---
+++ axltempl/composer.py
@@ -0,0 +1,169 @@
+"""
+Composer support functions
+"""
+
+import json
+import pkgutil
+import shutil
+import subprocess
+import sys
+
+from . import util
+
+
+def is_present():
+    """
+    Check if composer is present
+    """
+    return shutil.which("composer") is not None
+
+
+def get_version():
+    """
+    Attempt to get composer version
+    """
+    if not is_present():
+        raise RuntimeError("Composer not installed")
+
+    composer_run = subprocess.run(
+        "composer --version", shell=True, check=False, capture_output=True
+    )
+
+    if not composer_run.stdout.startswith(b"Composer version "):
+        raise RuntimeError("Unexpected output from composer --version")
+
+    version_string = composer_run.stdout
+    end_index = version_string.find(b" ", 17)
+    version = version_string[17:end_index]
+
+    return version.decode("utf-8")
+
+
+def is_version_2():
+    """
+    Check if we have composer 2 installed
+    """
+    version = get_version()
+    return version[0] == "2"
+
+
+def run_install():
+    """
+    Run composer install and handle errors
+    """
+    if not is_present():
+        util.write_error("Cannot find composer. Aborting...")
+        sys.exit(4)
+
+    composer_run = subprocess.run(
+        "composer install --ignore-platform-reqs -o", shell=True, check=False
+    )
+    if composer_run.returncode != 0:
+        util.write_error("Error when running 'composer install'. Aborting...")
+        util.write_error("Make sure you have set an adequate PHP memory limit.")
+        util.write_warning(
+            "Read {} for more details".format(
+                "https://getcomposer.org/doc/articles/troubleshooting.md#memory-limit-errors"
+            )
+        )
+        sys.exit(4)
+
+
+def get_drupal_template(name, description, core, core_version, docroot, cache_service):
+    """
+    Get the composer template from package and modify it as per given options
+    """
+    composer = json.loads(pkgutil.get_data(__name__, "files/drupal/composer.json"))
+    composer["name"] = name
+    composer["description"] = description
+    composer["extra"]["drupal-scaffold"]["locations"]["web-root"] = docroot + "/"
+    composer["extra"]["installer-paths"] = {
+        docroot + "/core": ["type:drupal-core"],
+        docroot + "/libraries/{$name}": ["type:drupal-library"],
+        docroot + "/modules/contrib/{$name}": ["type:drupal-module"],
+        docroot + "/profiles/contrib/{$name}": ["type:drupal-profile"],
+        docroot + "/themes/contrib/{$name}": ["type:drupal-theme"],
+        "drush/Commands/contrib/{$name}": ["type:drupal-drush"],
+        docroot + "/modules/custom/{$name}": ["type:drupal-custom-module"],
+        docroot + "/themes/custom/{$name}": ["type:drupal-custom-theme"],
+    }
+
+    if core == "core":
+        composer["require"]["drupal/core"] = core_version
+    if core == "recommended":
+        composer["require"]["drupal/core-recommended"] = core_version
+    if cache_service == "redis":
+        composer["require"]["drupal/redis"] = "^1.4"
+    if cache_service == "memcache":
+        composer["require"]["drupal/memcache"] = "^2.0"
+    composer["require"]["drupal/core-composer-scaffold"] = core_version
+    return composer
+
+
+def sort_packages(composer):
+    """
+    Sort the packages in a composer array
+    """
+    composer["require"] = util.sort_dictionary_by_keys(composer["require"])
+    composer["require-dev"] = util.sort_dictionary_by_keys(composer["require-dev"])
+    return composer
+
+
+class ComposerVersion:
+    """
+    Utility class to represent a composer package version
+    """
+
+    def __init__(self, name, version=""):
+        parts = name.split("/")
+        if len(parts) != 2:
+            raise ValueError(f"{name} is not a valid composer package name")
+        self.name = name
+        self.package_vendor = parts[0]
+        self.package_name = parts[1]
+
+        if " " in version and not ("'" in version or '"' in version):
+            version = f'"{version}"'
+        self.version = version
+
+    @staticmethod
+    def from_package_string(package_string):
+        """
+        Parse a composer package version string into a ComposerVersion type
+        """
+        if ":" in package_string:
+            name, version = package_string.split(":")
+        else:
+            name = package_string
+            version = ""
+
+        parts = name.split("/")
+        if len(parts) == 1:
+            name = "drupal/" + name
+        elif len(parts) != 2:
+            raise ValueError(f"{package_string} is not a valid composer package name")
+        return ComposerVersion(name, version)
+
+    def get_name(self):
+        """
+        Get the name of the composer package
+        """
+        return self.name
+
+    def get_package_vendor(self):
+        """
+        Get the name of the package vendor, e.g., "drupal" in "drupal/module"
+        """
+        return self.package_vendor
+
+    def get_package_name(self):
+        """
+        Get the name of the package name, e.g., "module" in "drupal/module"
+        """
+        return self.package_name
+
+    def get_version(self):
+        """
+        Get the version of the composer package (if any)
+        """
+        return self.version
--- axltempl/drupal.py
+++ axltempl/drupal.py
@@ -15,6 +15,7 @@ from . import click_types
 from . import gitlab
 from . import lando
 from . import util
+from . import composer
 
 
 DEFAULT_CORE_VERSION = "^8.9.0"
@@ -74,7 +75,7 @@ DEFAULT_CORE_VERSION = "^8.9.0"
     is_flag=True,
 )
 def main(
-    name: util.ComposerVersion,
+    name: composer.ComposerVersion,
     directory,
     description,
     core_package,
@@ -91,7 +92,8 @@ def main(
     Create a Drupal site template with NAME.
     Where NAME is the name of your application package (e.g., axelerant/site)
     """
-    ensure_memory_limit()
+    if not composer.is_version_2():
+        ensure_memory_limit()
 
     if directory == "":
         directory = name.get_package_name()
@@ -109,7 +111,7 @@ def main(
         cache_service=cache,
     )
 
-    run_composer_install()
+    composer.run_install()
 
     settings_file = ensure_settings_file(docroot)
     modify_settings_file(
@@ -204,28 +206,6 @@ def prepare_base_directory(directory, force):
         sys.exit(2)
 
 
-def run_composer_install():
-    """
-    Run composer install and handle errors
-    """
-    if shutil.which("composer") is None:
-        util.write_error("Cannot find composer. Aborting...")
-        sys.exit(4)
-
-    composer_run = subprocess.run(
-        "composer install --ignore-platform-reqs -o", shell=True, check=False
-    )
-    if composer_run.returncode != 0:
-        util.write_error("Error when running 'composer install'. Aborting...")
-        util.write_error("Make sure you have set an adequate PHP memory limit.")
-        util.write_warning(
-            "Read {} for more details".format(
-                "https://getcomposer.org/doc/articles/troubleshooting.md#memory-limit-errors"
-            )
-        )
-        sys.exit(4)
-
-
 def generate_drupal_files(
     name,
     description="",
@@ -237,7 +217,7 @@ def generate_drupal_files(
     """
     Generate Drupal files based on the given options
     """
-    composer = get_composer_template(
+    composer_template = composer.get_drupal_template(
         name=name,
         description=description,
         core=core,
@@ -245,9 +225,9 @@ def generate_drupal_files(
         docroot=docroot,
         cache_service=cache_service,
     )
-    composer = sort_composer_packages(composer)
+    composer_template = composer.sort_packages(composer_template)
     with open("composer.json", "w") as composer_file:
-        json.dump(composer, composer_file, indent=4)
+        json.dump(composer_template, composer_file, indent=4)
     util.write_file(".gitignore", get_gitignore(docroot))
 
     util.copy_package_file("files/drupal/load.environment.php", "load.environment.php")
@@ -271,58 +251,6 @@ def generate_drupal_files(
     util.write_file(docroot + "/themes/custom/.gitkeep", "")
 
     util.copy_package_file("files/drupal/renovate.json", "renovate.json")
-
-
-def get_composer_template(
-    name, description, core, core_version, docroot, cache_service
-):
-    """
-    Get the composer template from package and modify it as per given options
-    """
-    composer = json.loads(pkgutil.get_data(__name__, "files/drupal/composer.json"))
-    composer["name"] = name
-    composer["description"] = description
-    composer["extra"]["drupal-scaffold"]["locations"]["web-root"] = docroot + "/"
-    composer["extra"]["installer-paths"] = {
-        docroot + "/core": ["type:drupal-core"],
-        docroot + "/libraries/{$name}": ["type:drupal-library"],
-        docroot + "/modules/contrib/{$name}": ["type:drupal-module"],
-        docroot + "/profiles/contrib/{$name}": ["type:drupal-profile"],
-        docroot + "/themes/contrib/{$name}": ["type:drupal-theme"],
-        "drush/Commands/contrib/{$name}": ["type:drupal-drush"],
-        docroot + "/modules/custom/{$name}": ["type:drupal-custom-module"],
-        docroot + "/themes/custom/{$name}": ["type:drupal-custom-theme"],
-    }
-
-    if core == "core":
-        composer["require"]["drupal/core"] = core_version
-    if core == "recommended":
-        composer["require"]["drupal/core-recommended"] = core_version
-    if cache_service == "redis":
-        composer["require"]["drupal/redis"] = "^1.4"
-    if cache_service == "memcache":
-        composer["require"]["drupal/memcache"] = "^2.0"
-    composer["require"]["drupal/core-composer-scaffold"] = core_version
-    return composer
-
-
-def sort_composer_packages(composer):
-    """
-    Sort the packages in a composer array
-    """
-    composer["require"] = sort_dictionary_by_keys(composer["require"])
-    composer["require-dev"] = sort_dictionary_by_keys(composer["require-dev"])
-    return composer
-
-
-def sort_dictionary_by_keys(input_dict):
-    """
-    Sort the dictionary by keys in alphabetical order
-    """
-    sorted_dict = {}
-    for key in sorted(input_dict.keys()):
-        sorted_dict[key] = input_dict[key]
-    return sorted_dict
 
 
 def get_gitignore(docroot):
